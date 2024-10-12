fcd562e3b652706d74077c9662bceeb777825ef3
Jayant Kumar <118969771+jayant1796@users.noreply.github.com>
2024-10-12 12:20:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if __name__ == '__main__':
+    # Use the PORT environment variable, default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
     if request.method == "POST":
         url = request.form["url"]
         report = {}
