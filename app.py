27b7e6940dbe905d1385fc730263ac6c649c8817
Jayant Kumar <118969771+jayant1796@users.noreply.github.com>
2024-10-12 12:21:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,4 @@ def index():
 
     return render_template("index.html")
 
-if __name__ == '__main__':
-    # Use the PORT environment variable, default to 5000 if not set
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)  # Enable debug mode
+# Enable debug mode
